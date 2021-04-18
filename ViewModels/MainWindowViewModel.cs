53b39aef284c247d8b3ea7e41c28656951a76db0
Роман Терзиев <roman_terziev@hotmail.com>
2021-04-18 08:19:22 +0300

    Тестирование работы привязка данных ViewModel к окну

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Designer_Offer.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Designer_Offer.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "Designer Offer";
+
+        /// <summary>
+        /// Заголовок Окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
