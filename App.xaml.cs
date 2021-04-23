9825746d1d67301dd9c72f05f5589b8df73158f0
Dr_Brown <romanterziev@gmail.com>
2021-04-23 14:59:17 +0300

    mutex для исключения повторного запуска приложения

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -10,5 +11,20 @@ namespace Designer_Offer
 {
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "Designer Offer";
+
+            _mutex = new Mutex(true, appName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
