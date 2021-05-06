ad51973d9b38dc4d5be937f724b4528e48c02c80
Роман Терзиев <roman_terziev@hotmail.com>
2021-05-06 18:33:00 +0300

    добавление базового класса конвертер

---
+++ Infrastructure/Converters/Base/Converter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Designer_Offer.Infrastructure.Converters.Base
+{
+    internal abstract class Converter : IValueConverter
+    {
+        public abstract object Convert(object v, Type t, object p, CultureInfo c);
+
+        public virtual object ConvertBack(object va, Type t, object p, CultureInfo c)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+        }
+    }
+}
