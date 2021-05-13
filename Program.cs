610fad378a5b4f6dcdaa631bdbd2634647e173f7
Роман Терзиев <roman_terziev@hotmail.com>
2021-05-13 07:34:14 +0300

    Добавление новой точки входа в приложение

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Designer_Offer
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
