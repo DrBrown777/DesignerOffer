f81d407905a74e510e83ec61a416fe2ade571234
Роман Терзиев <roman_terziev@hotmail.com>
2021-05-21 23:43:23 +0300

    продолжение...

--- ViewModels/ProjectManagerViewModel.cs
+++ ViewModels/ProjectManagerViewModel.cs
@@ -1,21 +1,52 @@
 ﻿using Designer_Offer.Data;
 using Designer_Offer.Services.Interfaces;
 using Designer_Offer.ViewModels.Base;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Threading.Tasks;
 
 namespace Designer_Offer.ViewModels
 {
     class ProjectManagerViewModel : ViewModel
     {
         #region ПОЛЯ
+        static readonly string _title = " :: Управление проектами";
+        /// <summary>
+        /// Текущий пользователь
+        /// </summary>
         private readonly Employee CurrentUser;
-
+        /// <summary>
+        /// Текущая компания
+        /// </summary>
         private readonly Company CurrentCompany;
         #endregion
 
         #region СВОЙСТВА
-        private ObservableCollection<Client> _Clients;
+        private string _Title;
+        /// <summary>
+        /// Заголовок Окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
 
+        private string _Status;
+        /// <summary>
+        /// Статус программы
+        /// </summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        private ObservableCollection<Client> _Clients;
+        /// <summary>
+        /// Коллекция клиентов
+        /// </summary>
         public ObservableCollection<Client> Clients
         {
             get => _Clients;
@@ -23,7 +54,9 @@ namespace Designer_Offer.ViewModels
         }
 
         private ObservableCollection<Build> _Builds;
-
+        /// <summary>
+        /// Колекция обьектов клиентов
+        /// </summary>
         public ObservableCollection<Build> Builds
         {
             get => _Builds;
@@ -36,9 +69,13 @@ namespace Designer_Offer.ViewModels
                                                         IRepository<Client> repaClient, IRepository<Build> repaBuild)
         {
             //CurrentUser = repaUser.Get(employe.Id);
-            CurrentUser = repaUser.Get(22);
+            CurrentUser = repaUser.Get(21);
 
             CurrentCompany = repaCompany.Get((int)CurrentUser.Company_Id);
+
+            Title = CurrentCompany.Name + _title;
+
+            Status = CurrentUser.First_Name + " " + CurrentUser.Last_Name;
 
             Clients = new ObservableCollection<Client>(repaClient.Items);
 
--- ViewModels/WorkWindowViewModel.cs
+++ ViewModels/WorkWindowViewModel.cs
@@ -8,26 +8,6 @@ namespace Designer_Offer.ViewModels
     class WorkWindowViewModel : ViewModel
     {
         #region СВОЙСТВА
-        private string _Title = "Управление проектами";
-        /// <summary>
-        /// Заголовок Окна
-        /// </summary>
-        public string Title
-        {
-            get => _Title;
-            set => Set(ref _Title, value);
-        }
-
-        private string _Status = "Готов";
-        /// <summary>
-        /// Статус программы
-        /// </summary>
-        public string Status
-        {
-            get => _Status;
-            set => Set(ref _Status, value);
-        }
-
         private ViewModel _CurrentModel;
         /// <summary>
         /// Текущая дочерняя модель представления
