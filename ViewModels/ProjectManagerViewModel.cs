98e75434612b9d4ef1808503621f9e8383ff7a50
Dr_Brown <romanterziev@gmail.com>
2021-05-21 18:44:54 +0300

    начало привязок окна управления проектами

--- ViewModels/ProjectManagerViewModel.cs
+++ ViewModels/ProjectManagerViewModel.cs
@@ -1,22 +1,49 @@
 ﻿using Designer_Offer.Data;
 using Designer_Offer.Services.Interfaces;
 using Designer_Offer.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace Designer_Offer.ViewModels
 {
     class ProjectManagerViewModel : ViewModel
     {
+        #region ПОЛЯ
         private readonly Employee CurrentUser;
+
         private readonly Company CurrentCompany;
-        #region СВОЙСТВА
-
-
         #endregion
-        public ProjectManagerViewModel(IEntity employe, IRepository<Employee> repaUser, IRepository<Company> repaCompany)
+
+        #region СВОЙСТВА
+        private ObservableCollection<Client> _Clients;
+
+        public ObservableCollection<Client> Clients
         {
-            CurrentUser = repaUser.Get(employe.Id);
+            get => _Clients;
+            set => Set(ref _Clients, value);
+        }
+
+        private ObservableCollection<Build> _Builds;
+
+        public ObservableCollection<Build> Builds
+        {
+            get => _Builds;
+            set => Set(ref _Builds, value);
+        }
+        #endregion
+
+        #region КОНСТРУКТОРЫ
+        public ProjectManagerViewModel(IEntity employe, IRepository<Employee> repaUser, IRepository<Company> repaCompany, 
+                                                        IRepository<Client> repaClient, IRepository<Build> repaBuild)
+        {
+            //CurrentUser = repaUser.Get(employe.Id);
+            CurrentUser = repaUser.Get(22);
 
             CurrentCompany = repaCompany.Get((int)CurrentUser.Company_Id);
+
+            Clients = new ObservableCollection<Client>(repaClient.Items);
+
+            Builds = new ObservableCollection<Build>(repaBuild.Items);
         }
+        #endregion
     }
 }
