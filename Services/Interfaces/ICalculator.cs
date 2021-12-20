364d2015906ed8c41eddfea93b6f86731a4381c2
Dr_Brown <romanterziev@gmail.com>
2021-12-20 12:44:19 +0200

    введение интерфейса для расчета цен

---
+++ Services/Interfaces/ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Designer_Offer.Services.Interfaces
+{
+    internal interface ICalculator
+    {
+        void CalculateOfferPrice(int offerId);
+        void CalculateOneProductPrice(int productId, int installId);
+        void CalculateOneInstallPrice(int productId, int installId);
+        void CalculatePartPrice(int partId);
+    }
+}
